23f471fc52d56d5d052467c81bf0345abb9912c8
Md Yeah Yea Hasan <107740712+MdHasan2533@users.noreply.github.com>
2023-10-12 00:52:45 +0530

    Time: 235 ms (77.31%), Space: 82.5 MB (77.31%) - LeetHub

--- 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.cpp
+++ 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
-        vector<int> start, end;
-        for (auto& t : flowers)
-            start.push_back(t[0]), end.push_back(t[1]);
-        sort(start.begin(), start.end());
-        sort(end.begin(), end.end());
-        vector<int> res;
-        for (int t : people) {
-            int started = upper_bound(start.begin(), start.end(), t) - start.begin();
-            int ended = lower_bound(end.begin(), end.end(), t) - end.begin();
-            res.push_back(started - ended);
+        int m = flowers.size();
+        int n = people.size();
+
+        vector<int> start;
+        vector<int> end;
+
+        for( int i = 0; i < m; i++){
+            start.push_back(flowers[i][0]);
+            end.push_back(flowers[i][1]);
         }
-        return res;
+
+        sort(start.begin(),start.end());
+        sort(end.begin(), end.end());
+
+        vector<int> ans;
+
+        for( int i = 0; i < n; i++){
+
+            int time = people[i];
+            int bloomedFlowers = upper_bound(start.begin(), start.end(), time) - start.begin();
+            int diedFlowers = lower_bound(end.begin(), end.end(), time) - end.begin();
+            ans.push_back(bloomedFlowers - diedFlowers);
+
+        }
+
+        return ans;
     }
 };
